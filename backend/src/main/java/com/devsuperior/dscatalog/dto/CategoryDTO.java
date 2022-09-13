9b4bc0de4b6e4a2498deaa565b444c3253d1e0e5
Renata Fleck <rmfleck@edu.unisinos.br>
2022-09-13 15:12:54 -0300

    DTO

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.entity.Category;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO implements Serializable {
+
+    private Long id;
+    private String name;
+
+    public CategoryDTO(Category entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+}
--- backend/src/main/java/com/devsuperior/dscatalog/service/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/service/CategoryService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.service;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entity.Category;
 import com.devsuperior.dscatalog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -14,7 +16,13 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return categoryRepository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> list = categoryRepository.findAll();
+
+        //Tranforma minha "list" em uma stream (forma mais fácil de trabalhar com coleções)
+        //Percorre cada elemento do "list" e instancia um objeto do tipo "categoryDTO" com base no item que está sendo mapeado do "list"
+        //Ao invés de usar expressão lamba usei method reference pois estou chamando apenas um método
+        //Transforma novamente em lista
+        return list.stream().map(CategoryDTO::new).toList();
     }
 }
